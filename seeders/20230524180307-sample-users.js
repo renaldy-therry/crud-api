196371f1107c008d7b13c4b7929e286f1d4e8ff0
Renaldy <renaldytherrydota@gmail.com>
2023-05-25 02:46:21 +0800

    fourth commit

---
+++ seeders/20230524180307-sample-users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   return await queryInterface.bulkInsert(
+    "Users",
+    [
+      {
+        firstName: "Kobe",
+        lastName: "Bryant",
+        email: "kobe@mamba.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ],
+    {}
+   );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return await queryInterface.bulkDelete("Users", null, {});
+  }
+};
